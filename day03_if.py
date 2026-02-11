4defe3e128a4282ab8447b49de971356b65dceba
SimonMa-PKU <18810504655@163.com>
2026-02-11 22:10:06 +0800

    Day 03 - if elif else and integer conversion

---
+++ day03_if.py
@@ -0,0 +1,19 @@
+# age = int(input("How old are you? "))
+
+# if age < 13:
+#     print("You are a child.")
+# elif age < 18:
+#     print("You are a teenager.")
+# else:
+#     print("You are an adult.")
+
+score = int(input("Enter your score: "))
+
+if score >= 90:
+    print("Your grade is A.")
+elif score >= 80:
+    print("Your grade is B.")
+elif score >= 60:
+    print("Your grade is C.")
+else:
+    print("Your grade is F.")
