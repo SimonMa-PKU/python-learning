cdcb5ff2b54becc330b5775e4283e7aecb279641
SimonMa-PKU <18810504655@163.com>
2026-02-13 21:41:56 +0800

    Day 05 - list and dynamic input

---
+++ day05_list.py
@@ -0,0 +1,32 @@
+# numbers = [10, 20, 30]
+
+# # print(numbers)
+# # print(numbers[0])
+# # print(numbers[1])
+# # print(numbers[2])
+
+# for num in numbers:
+#     print(num)
+
+# numbers = []
+
+# numbers.append(5)
+# numbers.append(10)
+# numbers.append(15)
+
+# print(numbers)
+
+count = int(input("How many numbers? "))
+
+numbers = []
+
+for _ in range(count):
+    num = float(input("Enter number: "))
+    numbers.append(num)
+
+total = sum(numbers)
+
+print(f"Numbers: {numbers}")
+print(f"Total is {total:.2f}")
+print(f"The max is {max(numbers)}")
+print(f"The min is {min(numbers)}")
